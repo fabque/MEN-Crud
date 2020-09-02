958e105f5e48a31ab9722b39e046071fd12b6007
Fabricio <fabque@gmail.com>
2020-09-02 12:55:12 -0300

    Tutorial Controller and model

---
+++ app/models/tutorial.model.js
@@ -0,0 +1,31 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+        {
+           title: String,
+           description: String,
+            published: Boolean
+        },
+            {timestamps: true}
+        );
+
+//If you use this app with a front-end that needs id field instead of _id,
+// you have to override toJSON method that map default object to a custom object
+    schema.method("toJSON", function() {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Tutorial = mongoose.model("tutorial", schema);
+    return Tutorial;
+};
+
+/*
+{
+    "title": "Js Tut#",
+    "description": "Description for Tut#",
+    "published": true,
+    "createdAt": "2020-02-02T02:59:31.198Z",
+    "updatedAt": "2020-02-02T02:59:31.198Z",
+    "id": "5e363b135036a835ac1a7da8"
+}*/
